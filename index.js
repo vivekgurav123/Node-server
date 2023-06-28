4c9e28af0500a38f3bd427172193e3df8010c681
vivekgurav123 <vivekgurav012@gmail.com>
2023-06-28 19:58:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// const jsonwebtoken = require("jsonwebtoken");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  const users = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Doe" },
+  ];
+
+  res.send(users)
+  console.log(res.send(users));
+});
+
+app.listen(4000, ()=>{
+  console.log('Server is running at http://localhost:4000')
+});
